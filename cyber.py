a834d11c231b7cdd9c8b5e99bba9aa2fe7138cfb
raxitzora <raxitzora@gmail.com>
2025-08-09 00:48:09 +0530

    changed origin

--- cyber.py
+++ cyber.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Cyber Chatbot using Groq + LLaMA 3")
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
